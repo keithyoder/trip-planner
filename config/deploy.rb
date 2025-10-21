1e5fdd25178cb9e92166b7db829dad30d900794b
keithyoder <yoder@tessi.com.br>
2025-10-21 13:51:15 -0300

    remove log

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, "trip-planner"
 set :repo_url, "https://github.com/keithyoder/trip-planner.git"
 
 # Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
