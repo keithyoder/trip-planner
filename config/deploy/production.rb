cd771871ba143edf2537823b620177fcd7a7d3e9
keithyoder <yoder@tessi.com.br>
2025-10-22 09:22:14 -0300

    compile assets on deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -61,3 +61,27 @@
 #   }
 
 server 'trip.local', user: 'yoder', roles: %w[app db web]
+
+namespace :deploy do
+  desc 'Run yarn install'
+  task :yarn_install do
+    on roles(:web) do
+      within release_path do
+        execute :yarn, 'install --production --frozen-lockfile'
+      end
+    end
+  end
+  
+  desc 'Build CSS'
+  task :build_css do
+    on roles(:web) do
+      within release_path do
+        execute :yarn, 'build:css'
+      end
+    end
+  end
+end
+
+# Run in this order
+before 'deploy:assets:precompile', 'deploy:yarn_install'
+after 'deploy:yarn_install', 'deploy:build_css'
