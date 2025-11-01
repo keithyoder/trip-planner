084943ae4c3e5d333a201e697606a52e06df1234
keithyoder <yoder@tessi.com.br>
2025-11-01 09:32:56 -0300

    add RabbitMQ inflection

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # Be sure to restart your server when you modify this file.
 
 # Add new inflection rules using the following format. Inflections
@@ -15,3 +16,13 @@
 # ActiveSupport::Inflector.inflections(:en) do |inflect|
 #   inflect.acronym "RESTful"
 # end
+
+Rails.autoloaders.each do |autoloader|
+  autoloader.inflector.inflect(
+    'rabbitmq' => 'RabbitMQ'
+  )
+end
+
+ActiveSupport::Inflector.inflections(:en) do |inflect|
+  inflect.acronym 'RabbitMQ'
+end
