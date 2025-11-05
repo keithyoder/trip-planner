49dfe04bfe5691aef12f563d9a72005a24024424
keithyoder <yoder@tessi.com.br>
2025-11-05 08:54:20 -0300

    lots of translations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def format_duration(duration)
+    return t('common.not_available') if duration.nil?
+
+    hours = duration.parts[:hours] || 0
+    minutes = duration.parts[:minutes] || 0
+
+    parts = []
+    parts << "#{hours} #{t('common.hour', count: hours)}" if hours.positive?
+    parts << "#{minutes} #{t('common.minute', count: minutes)}" if minutes.positive?
+
+    parts.join(" #{t('common.and')} ")
+  end
 end
