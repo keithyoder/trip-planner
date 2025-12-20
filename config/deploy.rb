fdd4fe9243f5d8af98d6c714bc2f045868177a1c
keithyoder <yoder@tessi.com.br>
2025-12-20 20:35:59 -0300

    update cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock '~> 3.19.2'
+lock '~> 3.20.0'
 
 set :application, 'trip-planner'
 set :repo_url, 'https://github.com/keithyoder/trip-planner.git'
